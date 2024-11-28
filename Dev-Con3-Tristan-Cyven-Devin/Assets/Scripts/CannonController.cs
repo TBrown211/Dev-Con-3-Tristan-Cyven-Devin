985009acce9841c6458468c746575c651b0485f3
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-11-28 15:53:26 -0500

    Camera Movement Works!

--- Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonController.cs
+++ Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonController.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class CannonController : MonoBehaviour
 {
 
-    public float sensitivity;
+    public float senX = 100f;
+    public float senY = 100f;
 
     public Transform cannonMove;
-    public Camera cam;
 
-    float xRotation;
+    float xRotation = 0f;
+    float yRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float lookX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        float lookY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        float lookX = Input.GetAxisRaw("Mouse X") * senX * Time.deltaTime;
+        float lookY = Input.GetAxisRaw("Mouse Y") * senY * Time.deltaTime;
 
-        xRotation -= lookX;
+        yRotation += lookX;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        xRotation -= lookY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        cannonMove.Rotate(Vector3.up * lookX);
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
+        cannonMove.rotation = Quaternion.Euler(0f, yRotation, 0f);
+
+
     }
 }
