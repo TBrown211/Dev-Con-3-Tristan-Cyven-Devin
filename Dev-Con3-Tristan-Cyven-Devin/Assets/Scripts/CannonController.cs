88db055469818b507593a065e891d79b25885222
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-11-28 21:36:22 -0500

    Cannon Version 1 is ready

--- Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonController.cs
+++ Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonController.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-
+    //Sensestivity
     public float senX = 100f;
     public float senY = 100f;
 
     public Transform cannonMove;
-
+    //Roatations of the x and y axis
     float xRotation = 0f;
     float yRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
+        //This will lock the cursor to the center of the screen and make it invisible
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -22,15 +23,17 @@ public class CannonController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float lookX = Input.GetAxisRaw("Mouse X") * senX * Time.deltaTime;
+        //Gets the input for mouse movement
+        float lookX = Input.GetAxisRaw("Mouse X") * senX * Time.deltaTime; 
         float lookY = Input.GetAxisRaw("Mouse Y") * senY * Time.deltaTime;
 
-        yRotation += lookX;
-        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+        yRotation += lookX; //X mouse movement
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f); 
 
-        xRotation -= lookY;
+        xRotation -= lookY;//Y mouse movement
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        //Rotates the camera
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0f);
         cannonMove.rotation = Quaternion.Euler(0f, yRotation, 0f);
 
--- Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonFire.cs
+++ Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonFire.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class CannonFire : MonoBehaviour
 {
-    public GameObject cannonBallPrefab;
-    public Transform firePoint;
+    public GameObject cannonBallPreFab;
 
-    void Start()
+    public float shootForce;
+
+    public float timeBetweenShooting;
+    public float timeBetweenShots;
+
+    public Camera cannonCam;
+    public Transform attackPoint;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ShootCannon();
+        }
     }
 
-    void Update()
+    void ShootCannon()
     {
-        
+        Ray ray = cannonCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray,out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(75);
+        }
+
+        Vector3 cannonBallDirection = targetPoint - attackPoint.position;
+
+        GameObject newCannonBall = Instantiate(cannonBallPreFab, attackPoint.position, Quaternion.identity);
+        newCannonBall.transform.forward = cannonBallDirection.normalized;
+
+        newCannonBall.GetComponent<Rigidbody>().AddForce(cannonBallDirection.normalized * shootForce, ForceMode.Impulse);
     }
 }
