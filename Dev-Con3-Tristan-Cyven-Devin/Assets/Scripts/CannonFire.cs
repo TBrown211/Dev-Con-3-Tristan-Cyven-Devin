7aa12af3c5a60fa42a1ba0417d335d8657cd4b1d
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-11-28 20:14:04 -0500

    Beginning to get the Cannon to fire

---
+++ Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonFire.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonFire : MonoBehaviour
+{
+    public GameObject cannonBallPrefab;
+    public Transform firePoint;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
