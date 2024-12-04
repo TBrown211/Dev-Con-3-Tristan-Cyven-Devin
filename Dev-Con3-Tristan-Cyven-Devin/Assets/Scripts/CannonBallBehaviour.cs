9047e1b3c4e40c6a614633d74294574e5bd7f3d6
TBrown211 <150560857+TBrown211@users.noreply.github.com>
2024-12-04 16:53:19 -0500

    Cannonballs will now disappear after a certain amount of time

--- Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonBallBehaviour.cs
+++ Dev-Con3-Tristan-Cyven-Devin/Assets/Scripts/CannonBallBehaviour.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CannonBallBehaviour : MonoBehaviour
 {
+    public float maxLifeTime = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,10 @@ public class CannonBallBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        maxLifeTime -= Time.deltaTime;
+        if (maxLifeTime < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
